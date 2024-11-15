5fbe331deb43d6e59fb016084286619dc1b58f48
jules.poissonnet <jules.poissonnet@bedrockstreaming.com>
2024-11-15 19:37:40 +0100

    🚧 search with pagefind

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [pagefind()],
+});
